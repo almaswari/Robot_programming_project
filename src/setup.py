3c59fb52bbe805b69701a2dd75ab85283cc75dd7
Mohammed_Almaswary <almaswari1134@gmail.com>
2026-01-23 08:57:07 +0300

    Final verified submission: Modular structure tested and buildable in Docker

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,4 @@
 from setuptools import find_packages, setup
-from setuptools import find_packages, setup
 import os
 from glob import glob
 
@@ -8,29 +7,27 @@ package_name = 'quadrotor_sim'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    # We look for packages directly in the current folder (src)
+    packages=find_packages(), 
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('src/launch/*')), # Changed
-        (os.path.join('share', package_name, 'model'), glob('src/models/*')), # Changed
-        (os.path.join('share', package_name, 'parameters'), glob('src/parameters/*')), # Changed
+        # These paths are now relative to where setup.py is (inside src/)
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'model'), glob('models/*')),
+        (os.path.join('share', package_name, 'parameters'), glob('parameters/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ky_ode',
-    maintainer_email='ky_ode@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='Student',
+    maintainer_email='student@example.com',
+    description='Quadrotor Simulation',
+    license='MIT',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'position_controller = src.controllers.position_controller:main', # Changed
-            'data_plotter = src.plotters.data_plotter:main', # Changed
+            'position_controller = controllers.position_controller:main',
+            'data_plotter = plotters.data_plotter:main',
         ],
     },
 )
